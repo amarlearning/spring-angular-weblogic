d1d5799ef704a5e9b1a80fcaef3bde8f80dea5ff
Amar Prakash Pandey <amar.om1994@gmail.com>
2017-12-22 01:51:53 +0530

    initial commit

---
+++ src/main/java/com/src/DMutilities/DMutilitiesApplication.java
@@ -0,0 +1,12 @@
+package com.src.DMutilities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DMutilitiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DMutilitiesApplication.class, args);
+	}
+}
---
+++ src/test/java/com/java/src/DMutilities/DMutilitiesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.java.src.DMutilities;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DMutilitiesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
